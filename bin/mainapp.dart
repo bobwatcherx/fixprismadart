1119a9b6b064615cbedd3c32f43fc884d25da1ca
root <bobwatcherx@gmail.com>
2022-10-25 15:36:32 +0700

    fixed

---
+++ bin/mainapp.dart
@@ -0,0 +1,25 @@
+// import 'package:mainapp/mainapp.dart' as mainapp;
+
+// void main(List<String> arguments) {
+//   print('Hello world: ${mainapp.calculate()}!');
+// }
+
+import "package:mainapp/prisma_client.dart";
+
+final PrismaClient prisma = PrismaClient();
+
+void main() async {
+    try {
+     final User user = await prisma.user.create(
+        data: UserCreateInput(
+            name: "Seven",
+            email: "seven@odroe.com",
+        ),
+     );
+     print(user.toJson());
+    } catch(e) {
+        print(e.toString());
+    } finally {
+        await prisma.$disconnect();
+    }
+}
